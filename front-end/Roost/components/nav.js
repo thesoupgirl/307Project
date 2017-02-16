0eb53ba29111be57a3c9f109b806f8ef875c61f9
dpiotti@purdue.edu <dpiotti@purdue.edu>
2017-02-15 22:31:32 -0500

    basic pages working

--- front-end/Roost/components/nav.js
+++ front-end/Roost/components/nav.js
@@ -3,6 +3,12 @@ import { Container, Content, Button, Text, Footer,
 Icon, FooterTab, Header, View, Left, Body, Title, Right,
 DeckSwiper, Card, CardItem, Thumbnail} from 'native-base';
 import SwipeActivities from './swipeActivities.js';
+import Categories from './categories.js';
+import AddActivity from './addActivity.js';
+import MessageThreads from './messageThreads.js';
+import Profile from './Profile.js';
+
+
 //import getTheme from './native-base-theme';
 import {
   AppRegistry,
@@ -17,25 +23,26 @@ export default class Nav extends Component {
   constructor(props) {
         super(props)
         this.state = {
-          page: 'explore'
+          page: 'explore',
+          active: true
             
         }
 
         this.selectedPage = this.selectedPage.bind(this)
-        this.handleExplore = this.handleExplore.bind(this)
        
     }
 
-    handleExplore() {
-      //this.setState({page: 'explore'})
-    }
-
     selectedPage() {
-      if (this.state.page === 'hi')
-        return (<Text>hi</Text>)
-      else if (this.state.page === 'explore')
-        return <SwipeActivities/>
-      return <Text>nothing</Text>
+        if (this.state.page === 'explore')
+            return <SwipeActivities/>
+        else if (this.state.page === 'categories')
+            return <Categories/>
+        else if (this.state.page === 'add')
+            return <AddActivity/>
+        else if (this.state.page === 'messages')
+            return <MessageThreads/>
+        else if (this.state.page === 'profile')
+            return <Profile/>
     }
 
        render() {
@@ -43,9 +50,7 @@ export default class Nav extends Component {
             <Container>
                 <Header>
                     <Left>
-                        <Button transparent>
-                            <Icon name='menu' />
-                        </Button>
+                        
                     </Left>
                     <Body>
                        <Text>Roost</Text>
@@ -58,19 +63,29 @@ export default class Nav extends Component {
                 </Content>
                 <Footer>
                       <FooterTab>
-                          <Button active={this.state.page === 'explore'} onPress={this.handleExplore()}>
+                          <Button active={this.state.page === 'explore'}
+                                  onPress={() => {this.setState({ page: 'explore' })
+                                  this.selectedPage()}}>
                               <Icon name="globe" />
                           </Button>
-                          <Button>
+                          <Button active={this.state.page === 'categories'}
+                                  onPress={() => {this.setState({ page: 'categories' })
+                                  this.selectedPage()}}>
                               <Icon name="folder" />
                           </Button>
-                          <Button>
+                          <Button active={this.state.page === 'add'}
+                                  onPress={() => {this.setState({ page: 'add' })
+                                  this.selectedPage()}}>
                               <Icon name="add-circle" />
                           </Button>
-                          <Button >
+                          <Button active={this.state.page === 'messages'}
+                                  onPress={() => {this.setState({ page: 'messages' })
+                                  this.selectedPage()}}>
                               <Icon name="chatboxes" />
                           </Button>
-                          <Button>
+                          <Button active={this.state.page === 'profile'}
+                                  onPress={() => {this.setState({ page: 'profile' })
+                                  this.selectedPage()}}>
                               <Icon name="person" />
                           </Button>
                       </FooterTab>
