c7f65961d1cbc36b8733e6cc4268d8ccb7396527
Joseph Khawly <joseph.khawly@gmail.com>
2017-02-19 14:51:15 -0500

    Set up controller for API's user routes

---
+++ RoostApp/src/RoostApp/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RoostApp.Controllers
+{
+    [Route("api/user")]
+    public class UserController : Controller
+    {
+        // PUT: /api/user/{id}/updateuser
+        // Update user info
+        [HttpPut("{id:int}/updateuser")]
+        public IActionResult updateUser()
+        {
+            return View();
+        }
+
+        // POST: /api/user/create
+        [HttpPost("create")]
+        public IActionResult createUser()
+        {
+            return View();
+        }
+    }
+}
