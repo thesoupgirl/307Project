2da1a0ff64471ee34f78649bc64b02f62158696b
Joseph Khawly <joseph.khawly@gmail.com>
2017-02-26 17:33:54 -0500

    Added endpoint to save settings in UserController

--- RoostApp/src/RoostApp/Controllers/UserController.cs
+++ RoostApp/src/RoostApp/Controllers/UserController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Amazon.DynamoDBv2.Model;
 
 namespace RoostApp.Controllers
 {
     [Route("api/user")]
     public class UserController : Controller
     {
+
+        DBHelper db = new DBHelper();
+
         // PUT: /api/user/{id}/updateuser
         // Update user info
-        [HttpPut("{id:int}/updateuser")]
+        [HttpPut("{id}/updateuser")]
         public IActionResult UpdateUser()
         {
             return View();
@@ -23,6 +27,22 @@ namespace RoostApp.Controllers
         public IActionResult CreateUser()
         {
             return View();
+        }
+
+        // POST: /api/user/{id}/settings/update
+        [HttpPost("{id}/savesettings")]
+        // Saves user settings in DB
+        public async void SaveSettings()
+        {
+            await db.client.PutItemAsync(
+                tableName: "User",
+                item: new Dictionary<string, AttributeValue>
+                {
+                    // Save the settings as a string
+                    {"UserId", new AttributeValue {S = "{id}"} },
+                    {"Settings", new AttributeValue {S = ""} }
+                }
+            );
         }
     }
 }
