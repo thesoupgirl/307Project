b5e663181f288f425cd2fbfca3386725fbc31d20
Joseph Khawly <joseph.khawly@gmail.com>
2017-03-01 22:10:08 -0500

    Added exception handling for some routes

--- Roost/Controllers/UserController.cs
+++ Roost/Controllers/UserController.cs
@@ -51,42 +51,59 @@ namespace RoostApp.Controllers
 
         // POST: /api/user/create
         [HttpPost("create")]
+        [HttpGet("create")]
         // Create a new user
-        public async void CreateUser()
+        public async Task<string> CreateUser()
         {
-            await db.client.PutItemAsync(
-                tableName: "User",
-                item: new Dictionary<string, AttributeValue>
-                {
-                    {"userId", new AttributeValue {S = ""} },
-                    {"displayName", new AttributeValue {S = ""} }
-                }
-            );
+            try
+            {
+                var response = await db.client.GetItemAsync(
+                    tableName: "User",
+                    key: new Dictionary<string, AttributeValue>
+                    {
+                        {"userId", new AttributeValue {S = "777"} },
+                        {"displayName", new AttributeValue {S = "joe"} }
+                    }
+                );
+
+                return response.Item["userId"].S;
+            } catch (Exception)
+            {
+                return "User not found.";
+            }
         }
 
         // POST: /api/user/{id}
         [HttpPost("{id}")]
+        [HttpGet("{id}")]
         // Saves user settings in DB
-        public async void SaveSettings(string id)
+        public async Task<string> SaveSettings(string id)
         {
+            string settings = Response.Body.ToString();
 
-            await db.client.UpdateItemAsync(
-                tableName: "User",
-                key: new Dictionary<string, AttributeValue>
-                {
-                    {"userId", new AttributeValue {S = id} },
-                    {"displayName", new AttributeValue {S = "Hello world"} }
-                },
-
-                attributeUpdates: new Dictionary<string, AttributeValueUpdate>
-                {
+            try
+            {
+                var response = await db.client.UpdateItemAsync(
+                    tableName: "User",
+                    key: new Dictionary<string, AttributeValue>
                     {
-                        "settings", new AttributeValueUpdate(new AttributeValue {S = "Distance: 15mi"}, AttributeAction.PUT)
+                        {"userId", new AttributeValue {S = id} },
+                        {"displayName", new AttributeValue {S = "Hello world"} }
+                    },
 
+                    attributeUpdates: new Dictionary<string, AttributeValueUpdate>
+                    {
+                        {
+                            "settings", new AttributeValueUpdate(new AttributeValue {S = settings}, AttributeAction.PUT)
+                        }
                     }
-                }
-            );
-           
+                );
+                return "Settings have been updated.";
+            } catch (Exception)
+            {
+                return "Error: something went wrong.";
+            }
+            
         }
     }
 }
