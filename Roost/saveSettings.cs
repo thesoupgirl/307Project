e8eb5ebba7884ee351d212a93ccf6db8a44de3b6
amring <amring@purdue.edu>
2017-03-03 00:22:20 -0500

    began saving settings
    basis for method saving settings to database

---
+++ Roost/saveSettings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Amazon.DynamoDBv2.Model;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using RoostApp.Controllers;
+using RoostApp;
+
+namespace Roost
+{
+
+    public class SettingsSaver
+    {
+        DBHelper db = new DBHelper();
+
+        
+        public void saveSettings(string id, string settingsStr)
+        {
+            //save settingStr
+            //find user by id
+            DynamoDBContext context = new DynamoDBContext(db.client);
+            context.LoadAsync(new Dictionary<string, AttributeValue> { { "userID", new AttributeValue { S = id } } });
+            
+            context.SaveAsync(settingsStr);
+        }
+        
+
+        
+    }
+
+}
