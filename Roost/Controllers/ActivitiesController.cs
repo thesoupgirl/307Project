720fcd856f4d698ef8309cb2c911c9054c4168e3
Joseph Khawly <joseph.khawly@gmail.com>
2017-03-08 18:30:48 -0500

    Added endpoint for activity creation

--- Roost/Controllers/ActivitiesController.cs
+++ Roost/Controllers/ActivitiesController.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using Amazon.DynamoDBv2.Model;
 
 namespace RoostApp.Controllers
 {
     [Route("api/activities")]
     public class ActivitiesController : Controller
     {
+
+        DBHelper db = new DBHelper();
+
         // GET: /api/activities/{id}/{dist}
         // Gets list of activities within certain radius of user
         [HttpGet("{id}/{dist}")]
@@ -23,6 +28,39 @@ namespace RoostApp.Controllers
         public IActionResult FindActivitiesByCategory(string id, string dist)
         {
             return View();
+        }
+
+        // POST: /api/activities/add
+        [HttpPost("add")]
+        public async Task<HttpResponseMessage> AddActivity()
+        {
+            try
+            {
+
+                await db.client.PutItemAsync(
+                    tableName: "RoostActivities",
+                    item: new Dictionary<String, AttributeValue>
+                    {
+                        // The unique activity id
+                        {"ActivityId", new AttributeValue { S = ""} }, 
+
+                        // The number of members in the group
+                        {"NumMembers", new AttributeValue { N = "1"} },
+
+                        // The date the group was created
+                        {"CreatedDate", new AttributeValue { S = System.DateTime.Today.ToString()}  }
+                    }
+                );
+
+                Response.StatusCode = 200;
+                HttpResponseMessage response = new HttpResponseMessage();
+                return response;
+            } catch (Exception)
+            {
+                Response.StatusCode = 400;
+                HttpResponseMessage response = new HttpResponseMessage();
+                return response;
+            }
         }
 
         // POST: /api/activities/{id}/createactivity
