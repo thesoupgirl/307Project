551442efc1fa4179c6c702eb0533d1a35d25cb37
Zhuoya Chen <zhuoyachen526@gmail.com>
2017-04-05 12:16:52 -0400

    tinder cards

--- front-end/Roost/components/tindercards.js
+++ front-end/Roost/components/tindercards.js
@@ -1,4 +1,4 @@
-// Tinder.js 
+// tindercards.js 
 'use strict';
 
 import React from 'react';
@@ -21,8 +21,8 @@ let Card = React.createClass({
 let NoMoreCards = React.createClass({
   render() {
     return (
-      <View style={styles.noMoreCards}>
-        <Text>No more cards</Text>
+     <View style={[styles.card, {backgroundColor: this.props.backgroundColor}]}>
+        <Text>{this.props.text}</Text>
       </View>
     )
   }
@@ -40,13 +40,6 @@ const Cards = [
   {name: '9', image: 'https://media.giphy.com/media/3oEduJbDtIuA2VrtS0/giphy.gif'},
 ]
  
-const Cards2 = [
-  {name: '10', image: 'https://media.giphy.com/media/12b3E4U9aSndxC/giphy.gif'},
-  {name: '11', image: 'https://media4.giphy.com/media/6csVEPEmHWhWg/200.gif'},
-  {name: '12', image: 'https://media4.giphy.com/media/AA69fOAMCPa4o/200.gif'},
-  {name: '13', image: 'https://media.giphy.com/media/OVHFny0I7njuU/giphy.gif'},
-]
- 
 export default React.createClass({
   getInitialState() {
     return {
@@ -60,26 +53,6 @@ export default React.createClass({
   handleNope (card) {
     console.log("nope")
   },
-  cardRemoved (index) {
-    console.log(`The index is ${index}`);
- 
-    let CARD_REFRESH_LIMIT = 3
- 
-    if (this.state.cards.length - index <= CARD_REFRESH_LIMIT + 1) {
-      console.log(`There are only ${this.state.cards.length - index - 1} cards left.`);
- 
-      if (!this.state.outOfCards) {
-        console.log(`Adding ${Cards2.length} more cards`)
- 
-        this.setState({
-          cards: this.state.cards.concat(Cards2),
-          outOfCards: true
-        })
-      }
- 
-    }
- 
-  },
   render() {
     return (
       <SwipeCards
@@ -92,8 +65,7 @@ export default React.createClass({
         showNope={true}
  
         handleYup={this.handleYup}
-        handleNope={this.handleNope}
-        cardRemoved={this.cardRemoved} />
+        handleNope={this.handleNope} />
     )
   }
 })
